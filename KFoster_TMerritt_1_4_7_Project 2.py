be881c4ca9af9228185f16b6c99a4d0ee606ff8f
K10Fx <cuzkris10says@gmail.com>
2015-01-12 06:26:09 -0800

    Added Get_Images

---
+++ KFoster_TMerritt_1_4_7_Project 2.py
@@ -0,0 +1,36 @@
+import PIL
+import matplotlib.pyplot as plt
+import os.path  
+import PIL.ImageDraw
+
+def frame(original_image, percent_of_side):
+    width, height = original_image.size
+    border = int(percent_of_side*min(width, height))
+    
+def get_images(directory=None):
+    if directory == None:
+        directory = os.getcwd()
+    image_list = []
+    file_list = []
+    directory_list = os.listdir(directory)
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass
+    return image_list, file_list
+
+def frame_all_images(directory=None):
+    if directory==None:
+        directory = os.getcwd()
+    new_directory = os.path.join(directory, 'familyphotos')
+    try:
+        os.mkdir(new_directory)
+    except OSError:
+        pass
+    image_list, file_list = get_images(directory)
+    for n in range(len(image_list)):
+        filename, filetype = file_list[n].split('.')
